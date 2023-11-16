d0d63dff7cf4b420af352d510b5cd44d97fd2697
BrainnestgroupC5 <t.muzeffer270@gmail.com>
2023-11-16 15:05:44 +0200

    models were created for courses

--- smartedu_con/courses/models.py
+++ smartedu_con/courses/models.py
@@ -1,7 +1,14 @@
 from django.db import models
 
 
-
-
 class Course(models.Model):
-    pass
+    name = models.CharField(max_length=200, unique=True)
+    description = models.TextField(blank=True, null=True)
+    image = models.ImageField(upload_to='courses/%Y/%m/%d/', default='courses/default_course_image.png')
+    date = models.DateTimeField(auto_now=True)
+    available = models.BooleanField(default=True)
+
+
+    # in admin page after creation of the course then it will be shown by its name
+    def __str__(self):
+        return self.name
